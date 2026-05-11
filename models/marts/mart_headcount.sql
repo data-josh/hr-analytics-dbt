6495a4f2fcb0695791a9f9f072acb9cd4ee60b2c
josh1500s@gmail.com <josh1500s@gmail.com>
2026-05-11 23:16:34 +0000

    feat: added incremental mart_headcount model

--- models/marts/mart_headcount.sql
+++ models/marts/mart_headcount.sql
@@ -1,14 +1,37 @@
-select
-    snapshot_month,
-    department,
-    job_level,
-    country,
-    employment_type,
-    headcount,
-    remote_headcount,
-    active_headcount,
-    round(
-        safe_divide(remote_headcount, headcount) * 100, 1
-    ) as remote_pct
+{{
+    config(
+        materialized='incremental',
+        unique_key='snapshot_month_dept_key'
+    )
+}}
 
-from {{ ref('int_headcount_daily') }}
+with source as (
+
+    select
+        snapshot_month,
+        department,
+        job_level,
+        country,
+        employment_type,
+        headcount,
+        remote_headcount,
+        active_headcount,
+        round(
+            safe_divide(remote_headcount, headcount) * 100, 1
+        ) as remote_pct,
+        concat(
+            cast(snapshot_month as string), '_',
+            coalesce(department, 'unknown'), '_',
+            coalesce(job_level, 'unknown'), '_',
+            coalesce(country, 'unknown')
+        ) as snapshot_month_dept_key
+
+    from {{ ref('int_headcount_daily') }}
+
+    {% if is_incremental() %}
+        where snapshot_month > (select max(snapshot_month) from {{ this }})
+    {% endif %}
+
+)
+
+select * from source
