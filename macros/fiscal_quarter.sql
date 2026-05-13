57753749e18c8f44374926808a9b8e274b859221
josh1500s@gmail.com <josh1500s@gmail.com>
2026-05-13 23:30:14 +0000

    fix: simplify fiscal_quarter macro syntax

--- macros/fiscal_quarter.sql
+++ macros/fiscal_quarter.sql
@@ -1,8 +1,8 @@
-{% macro fiscal_quarter(date_column) %}
+{% macro fiscal_quarter(date_column) -%}
     case
-        when extract(month from {{ date_column }}) in (1, 2, 3)   then 'Q1'
-        when extract(month from {{ date_column }}) in (4, 5, 6)   then 'Q2'
-        when extract(month from {{ date_column }}) in (7, 8, 9)   then 'Q3'
+        when extract(month from {{ date_column }}) in (1, 2, 3)    then 'Q1'
+        when extract(month from {{ date_column }}) in (4, 5, 6)    then 'Q2'
+        when extract(month from {{ date_column }}) in (7, 8, 9)    then 'Q3'
         when extract(month from {{ date_column }}) in (10, 11, 12) then 'Q4'
     end
-{% endmacro %}
+{%- endmacro %}
