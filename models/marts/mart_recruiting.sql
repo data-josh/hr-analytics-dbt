dd57d16bbf852539b8024f3a42b9c1cfa5f0b5f7
josh1500s@gmail.com <josh1500s@gmail.com>
2026-05-16 16:08:31 +0000

    feat: fixed department normalization in stg_icims_jobs, fix mart_recruiting grouping

--- models/marts/mart_recruiting.sql
+++ models/marts/mart_recruiting.sql
@@ -11,7 +11,6 @@ select
     closed_date,
     salary_min,
     salary_max,
-    source,
     count(application_id)                           as total_applications,
     countif(furthest_stage_reached = 'Hired')       as total_hires,
     countif(is_rejected = true)                     as total_rejections,
@@ -24,6 +23,5 @@ select
     )                                               as hire_rate_pct,
     round(avg(days_to_outcome), 1)                  as avg_days_to_outcome,
     round(avg(days_from_post_to_apply), 1)          as avg_days_to_first_apply
-
 from {{ ref('int_recruiting_pipeline') }}
-group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+group by 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
